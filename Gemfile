323d1722863f30b014334ad94dcaf9f6843031db
David Ziegler <dzieglersf@gmail.com>
2012-04-02 15:20:46 -0700

    updated to use guard with RSpec 2, etc.

--- Gemfile
+++ Gemfile
@@ -1,21 +1,39 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
-gem 'sqlite3'
+gem 'jquery-rails', '2.0.0'
 
-gem 'gravatar_image_tag'
-gem 'will_paginate'
-gem 'jquery-rails'
+# gem 'gravatar_image_tag'
+# gem 'will_paginate'
 
 group :development do
-  gem 'rspec-rails', '2.0.1'
-#  gem 'faker'
+	gem 'sqlite3', '1.3.5'
+  gem 'rspec-rails', '2.9.0'
+	gem 'guard-rspec', '0.5.5'
+	gem 'growl'
+# gem 'faker'
+end
+
+group :assets do
+	gem 'sass-rails', '3.2.4'
+	gem 'coffee-rails', '3.2.2'
+	gem 'uglifier', '1.2.3'
 end
 
 group :test do
-  gem 'rspec', '2.0.1'
-  gem 'webrat', '0.7.1'
-  gem 'spork'
-#  gem 'factory_girl_rails'
+  gem 'rspec-rails', '2.9.0'
+ 	gem 'capybara', '1.1.2'
+ 	gem 'rb-fsevent', '0.4.3.1', :require => false
+ 	gem 'growl'
 end
+
+group :production do
+	gem 'pg', '0.12.2'
+end
+
+# gem 'rspec', '2.0.1'
+# gem 'webrat', '0.7.1'
+# gem 'spork'
+# gem 'factory_girl_rails'
+
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe PagesController do
-  render_views
+
+  describe "Get 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      visit 'help'
+      page.should have_selector("help",
+        :content => "Ruby on Rails Tutorial Sample App | Help")
+    end
+
+  end
+
 
   describe "GET 'about'" do
     it "should be successful" do
@@ -10,12 +24,13 @@ describe PagesController do
     end
 
     it "should have the right title" do
-      get 'about'
-      response.should have_selector("title",
+      visit 'about'
+      page.should have_selector("title",
 				:content => "Ruby on Rails Tutorial Sample App | About")
     end
 
   end
+
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -24,12 +39,13 @@ describe PagesController do
     end
 
     it "should have the right title" do
-      get 'home'
-      response.should have_selector("title", 
+      visit 'home'
+      page.should have_selector("title", 
 				:content => "Ruby on Rails Tutorial Sample App | Home")
     end
 
   end
+
 
   describe "GET 'contact'" do
     it "should be successful" do
@@ -38,8 +54,8 @@ describe PagesController do
     end
   
     it "should have the right title" do
-      get 'contact'
-      response.should have_selector("title",
+      visit 'contact'
+      page.should have_selector("title",
 				:content => "Ruby on Rails Tutorial Sample App | Contact")
     end
 
