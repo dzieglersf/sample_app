0959894d2af162e8493f05e5f68e6acf228fc00c
David Ziegler <dzieglersf@gmail.com>
2012-04-05 15:51:02 -0700

    edited tests, cleaned up some functions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,12 +28,22 @@ describe "UserPages" do
 			it "should not create a user" do
 				expect { click_button "Create my account" }.not_to change User, :count
 			end
+
+			describe "error messages" do
+
+				before { click_button "Create my account" }
+
+				it { should have_selector 'title', text: 'Sign up' }
+				it { should have_content 'error' }
+
+			end
+
 		end
 
 		describe "with valid information" do
 			before do
 				fill_in "Name",			 			with: "Example User"
-				fill_in "Email",					with: "example@railstutorial.org"
+				fill_in "Email",					with: "user@example.com"
 				fill_in "Password",				with: "yeehaw"
 				fill_in "Confirmation", 	with: "yeehaw"
 			end
@@ -42,6 +52,15 @@ describe "UserPages" do
 				expect do
 					click_button "Create my account"
 				end.to change(User, :count).by(1)
+			end
+
+			describe "afater saving the user" do
+				before { click_button "Create my account" }
+				let(:user) { User.find_by_email("user@example.com") }
+
+				it { should have_selector 'title', text: user.name }
+				it { should have_selector 'div.alert.alert-success', text: 'Welcome' }
+
 			end
 
 		end
